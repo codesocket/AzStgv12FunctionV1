ad71a010e68dba00ff699089c87a64a8bf5b70c2
Abdul Rehman <Abdul.Rehman@ey.com>
2019-12-06 16:43:36 -0500

    Add project files.

---
+++ AzStg12Function2/UploadBlobTimerFunction.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Text;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace AzStg12Function2
+{
+    public static class UploadBlobTimerFunction
+    {
+        static UploadBlobTimerFunction()
+        {
+            // Un-comment this line to override assembly version resolution
+            //ApplicationHelper.Startup();
+        }
+
+        [FunctionName("Function1")]
+        public static void Run([TimerTrigger("*/5 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            var connectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
+            var blobServiceClient = new BlobServiceClient(connectionString: connectionString);
+            var containerClient = blobServiceClient.GetBlobContainerClient(blobContainerName: "files");
+
+            containerClient.CreateIfNotExists();
+
+            var time = DateTime.Now.ToString();
+            var blobClient = containerClient.GetBlobClient(blobName: $"{time}.txt");
+
+            blobClient.Upload(content: new MemoryStream(Encoding.UTF8.GetBytes($"Test blob created at {time}"), 0, 12));
+        }
+    }
+}
